9988244aea7b376f503282ed2cf5eae294d9371f
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-01-22 09:39:04 +0000

    Add a public Cargo.toml parsing function, the crate is now usable end-to-end

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,11 @@ mod fields;
 mod raw_fields;
 mod resolver;
 
+pub use error::Error;
+use raw_fields::RawTomlFields;
 pub use resolver::Resolver;
 
-// fn from_toml(parsed_toml: toml::Table) {
-
-// }
+pub fn from_toml(workspace_root_cargo_toml: &str) -> Result<Resolver, crate::Error> {
+    let parsed: RawTomlFields = toml::from_str(workspace_root_cargo_toml)?;
+    Ok(parsed.resolve().resolver()?)
+}
