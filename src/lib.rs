16c079f6dcc3d95e052e5f1bd73b410e454aebb0
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-01-22 06:00:19 +0000

    Add Resolver enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 mod fields;
 mod raw_fields;
+mod resolver;
+
+pub use resolver::Resolver;
 
 // fn from_toml(parsed_toml: toml::Table) {
 
---
+++ src/resolver.rs
@@ -0,0 +1,5 @@
+pub enum Resolver {
+    V1,
+    V2,
+    V3,
+}
